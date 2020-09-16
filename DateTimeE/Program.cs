4941bdf36b855c3a63af0ec9436f03d3c246ead0
Prabhat <1228prabhat@gmail.com>
2020-09-16 21:20:06 +0530

    date time class practice

---
+++ DateTimeE/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DateTimeE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime datetime;
+            DateTime datetime1;
+            DateTime now = DateTime.Now;
+            DateTime birthDay = new DateTime(2020, 09, 28);
+           string input= Console.ReadLine();
+            if(DateTime.TryParse(input,out datetime))
+            {
+                datetime1 = datetime;
+                TimeSpan dayselapsed = now.Subtract(datetime1);
+                Console.WriteLine(dayselapsed.Days);
+            }
+            
+            Console.WriteLine(now);
+            
+        }
+      
+    }
+}
--- LAMDA_EXPRESSION/Program.cs
+++ LAMDA_EXPRESSION/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Security.Cryptography.X509Certificates;
 
@@ -8,19 +9,27 @@ namespace LAMDA_EXPRESSION
     {
         public delegate int math(int p);//public delegate , it is used as a pointer to method having same parameter as delegate
         public static int num = 4;
+        
         static void Main(string[] args)
         {
-            
+            Predicate<int> index1 = x => x == 4;//predicate with  lamda expression 
             math n2;
             math n1 = add;
             math n3 = multiply;
             n2 = n1;
             n2 += n3;
+            int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+            Array.ForEach( numbers,i => Console.WriteLine(i)); //lemda expression for for each loop in array
+            List<int> list = new List<int> { 1, 2, 3, 4, 5 }; 
+            list.ForEach( i => Console.WriteLine(i));
+            int index = list.FindIndex(index1);
+            Console.WriteLine(list[index]);
+            Console.WriteLine(list[3]);
             
-            Console.WriteLine(n2(4));
+            //Console.WriteLine(n2(4));
             math nc1 = num => num - 1; //lemda expression => is a lemda operator , in equation left side operator are arguments and right side is return value
             nc1(4);
-            Console.WriteLine(nc1(4));
+           // Console.WriteLine(nc1(4));
         }
         public static int add(int p)
         {
